559cba56b82d10667487bafe4ebf809175adb568
Davide Principi <davide.principi@nethesis.it>
2012-08-08 18:19:36 +0200

    Added translations..

---
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Mail_Filter.php
@@ -0,0 +1,6 @@
+<?php
+
+$L['Filter_Title'] = 'Filtro';
+$L['VirusCheckStatus_label'] = 'Controlla virus';
+$L['SpamCheckStatus_label'] = 'Controlla spam';
+$L['BlockAttachmentStatus_label'] = 'Blocca allegati';
