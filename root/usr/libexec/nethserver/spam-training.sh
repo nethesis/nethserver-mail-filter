81c722d61bae9e9bf463beb4d735c65969d5a812
Davide Principi <davide.principi@nethesis.it>
2012-11-21 18:01:28 +0100

    dovecot.conf template: imported spam_training fragment from project:nethserver-mail-server. Refs #1516 -- SpamAssassin's Bayesian classifier training

---
+++ root/usr/libexec/nethserver/spam-training.sh
@@ -0,0 +1,63 @@
+#!/bin/bash 
+
+#
+# NethServer -- spam-training.sh 
+#
+# Read a mail message from standard input and pass it to sa-learn.
+#
+# Copyright (C) 2012 Nethesis srl
+#
+
+exec >-
+
+function log {
+    local level=$1
+    shift
+    [ -x /usr/bin/logger ] && /usr/bin/logger -p "mail.${level}" "${PROG} (${USER})" $*;    
+}
+
+export LANG=C
+
+PROG=`basename $0`
+USER=$1
+ACTION=$2
+sa_learn=/usr/bin/sa-learn
+
+# If defined spamtrainers user group, require that the current user is
+# a member of it before going on.
+sa_learn_group=`/usr/bin/getent group spamtrainers`
+if [ $? -eq 0 ] && ! echo $sa_learn_group | \
+    /bin/cut -d : -f 4 | \
+    /bin/grep -q "\<${USER}\>"; then
+
+    log debug "Not a member of 'spamtrainers' group. No sa-learn occurs."
+    exit 0;
+fi
+
+# Ensure temporary spool file is deleted when the process terminates
+TEMPFILE=`/bin/mktemp /var/tmp/spam-training.XXXXXXXXXXX`
+trap "/bin/rm -f ${TEMPFILE}" EXIT SIGINT SIGHUP SIGTERM
+
+/bin/cat <&0 >>${TEMPFILE} 
+
+if [ $? -ne 0 ]; then
+    log err "Could not write to ${TEMPFILE}"
+    exit 2 
+fi
+
+if [ $ACTION == 'ham' ]; then
+    $sa_learn --ham ${TEMPFILE}
+elif [ $ACTION == 'spam' ]; then
+    $sa_learn --spam ${TEMPFILE}
+else 
+    log err "Action '${ACTION}' is not recognized"
+    exit 3
+fi
+
+if [ $? -ne 0 ]; then
+    log err "Message classification failed"
+    exit 1
+fi 
+
+log info "Message classified as ${ACTION}"
+
